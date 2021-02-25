96bbee7ed48c214729c71c5ee88c73f5bfcaa89e
Alex McLean <alex.mclean25@gmail.com>
2021-02-24 21:19:29 -0800

    Update brew.sh
    Actually installing home-brew

--- brew.sh
+++ brew.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Installing Homebrew."
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Finished installing Homebrew"
 
+echo "Updating Homebrew..."
 # Make sure we’re using the latest Homebrew.
 brew update
 brew upgrade
+echo "Finshed updating Homebrew..."
 
+echo "Installing Homebrew packages..."
 # Installs Brew
 brew install node
 brew install git
 
+echo "Homebrew casks..."
 # Installs Casks
 brew tap caskroom/cask
 
